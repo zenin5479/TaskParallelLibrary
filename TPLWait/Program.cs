38edb3de7fd4bb2399b859870621ff1226de339e
zenin5479 <zenin5479@gmail.com>
2024-01-13 01:25:45 +0300

    Update 0.4 Обновлен метод

---
+++ TPLWait/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+// Для использования AsyncResult
+
+namespace TPLWait
+{
+    class Program
+    {
+        static void MyTask()
+        {
+            for (int i = 0; i < 80; i++)
+            {
+                Thread.Sleep(25);
+                Console.Write(".");
+            }
+        }
+
+        static void Main()
+        {
+            Task task = new Task(MyTask);
+            task.Start();
+            Thread.Sleep(500);
+            Console.WriteLine("\ntask.IsCompleted = " + task.IsCompleted);
+
+            // Ожидание завершения асинхронной задачи.
+            // Вариант 1:
+            task.Wait();
+            // Вариант 2:
+            //while (!task.IsCompleted)
+            //    Thread.Sleep(100);
+            // Вариант 3: 
+            //IAsyncResult asynkResult = task as IAsyncResult;
+            //ManualResetEvent waitHandle = asynkResult.AsyncWaitHandle as ManualResetEvent;
+            //waitHandle.WaitOne();
+
+            Console.WriteLine("\ntask.IsCompleted = " + task.IsCompleted);
+
+            // Задержка
+            Console.ReadKey();
+        }
+    }
+}
