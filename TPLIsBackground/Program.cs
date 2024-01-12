ad4ca194fcd275827c313cf3d553a38baea26147
zenin5479 <zenin5479@gmail.com>
2024-01-13 01:09:19 +0300

    Update 0.2 Обновлен метод

---
+++ TPLIsBackground/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+// С завершением метода Main завершается недовыполненная задача MyTask
+// [завершается работа вторичного потока].
+// По умолчанию IsBackground == true
+
+namespace TPLIsBackground
+{
+    class Program
+    {
+        static void MyTask()
+        {
+            // Снять комментарий.
+            Thread.CurrentThread.IsBackground = false;
+            for (int i = 0; i < 80; i++)
+            {
+                Thread.Sleep(100);
+                Console.Write(".");
+            }
+        }
+
+        static void Main()
+        {
+            Task task = new Task(MyTask);
+            task.Start();
+            // Время на запуск задачи.
+            Thread.Sleep(500);
+            Console.WriteLine("\nMain завершен.");
+
+            // Задержка
+            Console.ReadKey();
+        }
+    }
+}
