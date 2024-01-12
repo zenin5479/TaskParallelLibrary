6b63dc0b8b8f078ebb31fd4c92f7e2e1d59040eb
zenin5479 <zenin5479@gmail.com>
2024-01-13 01:00:41 +0300

    Добавьте файлы проекта.

---
+++ TPLStatus/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+// TaskStatus - статусы задачи.
+
+namespace TPLStatus
+{
+    class Program
+    {
+        static void MyTask()
+        {
+            Thread.Sleep(1000);
+        }
+
+        static void Main()
+        {
+            Task task = new Task(MyTask);
+            // Задача не запущена.
+            Console.WriteLine("1. " + task.Status);
+            // Задача в процессе запуска.
+            task.Start();
+            Console.WriteLine("2. " + task.Status);
+            // Задача выполняется.
+            Thread.Sleep(1000);
+            Console.WriteLine("3. " + task.Status);
+            // Задача завершилась.
+            Thread.Sleep(1000);
+            Console.WriteLine("4. " + task.Status);
+
+            // Задержка
+            Console.ReadKey();
+        }
+    }
+}
