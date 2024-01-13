da1ae21b63a22f527a3af9527ffbfbb921829389
zenin5479 <zenin5479@gmail.com>
2024-01-13 12:11:02 +0300

    Update 1.1 Обновлен метод

---
+++ TPLReturn/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TPLReturn
+{
+    struct Context
+    {
+        public int A;
+        public int B;
+    }
+
+    internal class Program
+    {
+        // Метод который будет возвращать результат.
+        static int Sum(object arg)
+        {
+            int a = ((Context)arg).A;
+            int b = ((Context)arg).B;
+            Thread.Sleep(1000);
+            return a + b;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Основной поток запущен.");
+            Context context;
+            context.A = 3;
+            context.B = 5;
+            Task<int> task;
+
+            // 1 вариант
+            task = new Task<int>(Sum, context);
+            task.Start();
+            // 2 вариант
+            //TaskFactory<int> factory = new TaskFactory<int>();
+            //task = factory.StartNew(Sum, context);
+            // 3 вариант
+            //task = Task<int>.Factory.StartNew(Sum, context);
+            Console.WriteLine("Результат выполнения задачи Sum = " + task.Result);
+            Console.WriteLine("Основной поток завершен.");
+
+            // Задержка
+            Console.ReadKey();
+        }
+    }
+}
