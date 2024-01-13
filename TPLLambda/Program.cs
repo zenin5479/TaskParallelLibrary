0299c38df8523fca1fdd350130543a9c565a9cbd
zenin5479 <zenin5479@gmail.com>
2024-01-13 11:48:47 +0300

    Update 0.9 Обновлен метод

---
+++ TPLLambda/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TPLLambda
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Основной поток запущен.");
+            // Использование лямбда-оператора для определения задачи.
+            Task task = Task.Factory.StartNew(() =>
+            {
+                for (int i = 0; i < 80; i++)
+                {
+                    Thread.Sleep(20);
+                    Console.Write(".");
+                }
+            });
+            // Ожидание завершения задачи.
+            task.Wait();
+            // Освобождение задачи. 
+            task.Dispose();
+            Console.WriteLine("\nОсновной поток завершен.");
+
+            // Задержка
+            Console.ReadKey();
+        }
+    }
+}
